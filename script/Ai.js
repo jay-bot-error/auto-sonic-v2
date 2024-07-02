3b5fe104ddb15aa2130e33825e771e8ee5cdcd6a
Sory Ronald <165383634+SoryRonald@users.noreply.github.com>
2024-07-02 00:16:29 +0000

    Update Ai.js

--- script/Ai.js
+++ script/Ai.js
@@ -1,29 +1,38 @@
 const axios = require('axios');
 
 module.exports.config = {
-		name: "Ai",
-		version: 1.0,
-		credits: "OtinXSandip",
-		description: "AI",
-		hasPrefix: false,
-		usages: "{pn} [prompt]",
-		aliases: ["megan","AE"],
-		cooldown: 0,
+  name: "ai2",
+  version: "69",
+  role: 0,
+  credits: "OtinXSandip", // converted by kira
+  description: "ask AI",
+  usages: "ask <question>",
+  hasPrefix: false,
+  commandCategory: "ai",
+  cooldowns: 0
 };
+  
+module.exports.run = async function ({ api, event, args, message }) {
+  try {
+    const prompt = event.body.trim();
+    if (!prompt) {
+      await api.sendMessage({ body: "salut qu'es ce que je peux faire pour toi 👨🏻‍💻" }, event.threadID);
+      return;
+    }
+    api.setMessageReaction("🔎", event.messageID, (err) => {}, true);
+    const response = await axios.get(`https://sandipapi.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+    const answer = response.data.answer;
 
-module.exports.run = async function ({ api, event, args }) {
-		try {
-				const prompt = args.join(" ");
-				if (!prompt) {
-						await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
-						return;
-				}
+    await api.sendMessage({
+      body: `𝗕𝗢𝗧 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 | 🟢
+━━━━━━━━━━━━━━━━━━        
+${answer}
+━━━━━━━━━━━━━━━━━━\n\n- 𝚃𝚑𝚒𝚜 𝚋𝚘𝚝 𝚞𝚗𝚍𝚎𝚛 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝚋𝚢 𝚁𝚘𝚗𝚊𝚕𝚍-it\n• 𝗙𝗕𝗟𝗶𝗡𝗞: https://www.facebook.com/sory.ronald.alexandre`,
+    }, event.threadID);
 
-				const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
-				const answer = response.data.answer;
-
-				await api.sendMessage(answer, event.threadID);
-		} catch (error) {
-				console.error("Error:", error.message);
-		}
+  } catch (error) {
+    console.error("🔴 An error occurred while processing your request.\nPlease contact churchill abing for an error", error.message);
+    api.setMessageReaction("🔴", event.messageID, (err) => {}, true);
+  }
 };
