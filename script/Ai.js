87316b11d6d665fe6e78b4ef4edfdf4fffb01e6d
Sory Ronald <165383634+SoryRonald@users.noreply.github.com>
2024-07-19 14:46:30 +0000

    Update Ai.js

--- script/Ai.js
+++ script/Ai.js
@@ -1,33 +1,29 @@
 const axios = require('axios');
+
 module.exports.config = {
-  name: 'ai',
-  version: '1.0.0',
-  role: 0,
-  hasPrefix: false,
-  aliases: ['gpt', 'openai'],
-  description: "An AI command powered by GPT-4",
-  usage: "Ai [promot]",
-  credits: '𝗮𝗲𝘀𝘁𝗵𝗲𝗿',
-  cooldown: 3,
+		name: "Ai",
+		version: 1.0,
+		credits: "OtinXSandip",
+		description: "AI",
+		hasPrefix: false,
+		usages: "{pn} [prompt]",
+		aliases: ["megan","AE"],
+		cooldown: 0,
 };
-module.exports.run = async function({
-  api,
-  event,
-  args
-}) {
-  const input = args.join(' ');
-  if (!input) {
-    api.sendMessage(`Oui je suis là pour répondre à tes questions...👨🏻‍💻`, event.threadID, event.messageID);
-    return;
-  }
-  api.sendMessage(``, event.threadID, event.messageID);
-  try {
-    const {
-      data
-    } = await axios.get(`https://metoushela-rest-api-koak.onrender.com/api/gpt4o?context=hi`);
-    const response = data.response;
-    api.sendMessage('════════════════\n['+ response +'] \n════════════════ \n https://www.facebook.com/sory.ronald.alexandre', event.threadID, event.messageID);
-  } catch (error) {
-    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
-  }
+
+module.exports.run = async function ({ api, event, args }) {
+		try {
+				const prompt = args.join(" ");
+				if (!prompt) {
+						await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
+						return;
+				}
+
+				const response = await axios.get(`https://metoushela-rest-api-koak.onrender.com/api/gpt4o?context=hi`);
+				const answer = response.data.answer;
+
+				await api.sendMessage(answer, event.threadID);
+		} catch (error) {
+				console.error("Error:", error.message);
+		}
 };
