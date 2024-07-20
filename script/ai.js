0a8305927faca7b5b747e0c3b36d4c2698b4e76b
Sory Ronald <165383634+SoryRonald@users.noreply.github.com>
2024-07-20 01:23:29 +0000

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,29 +1,44 @@
+const fonts = {
+  a: "𝖺", b: "𝖻", c: "𝖼", d: "𝖽", e: "𝖾", f: "𝖿", g: "𝗀", h: "𝗁",
+  i: "𝗂", j: "𝗃", k: "𝗄", l: "𝗅", m: "𝗆", n: "𝗇", o: "𝗈", 
+  p: "𝗉", q: "𝗊", r: "𝗋", s: "𝗌", t: "𝗍", u: "𝗎", v: "𝗏", 
+  w: "𝗐", x: "𝗑", y: "𝗒", z: "𝗓" 
+};
+
 const axios = require('axios');
 
 module.exports.config = {
-		name: "ai",
-		version: 1.0,
-		credits: "OtinXSandip",
-		description: "AI",
-		hasPrefix: false,
-		usages: "{pn} [prompt]",
-		aliases: ["megan","AE"],
-		cooldown: 0,
+  name: "ai",
+  version: 1.0,
+  credits: "aesther",//Api OtinXsandip
+  description: "AI",
+  hasPrefix: false,
+  usages: "{pn} [prompt]",
+  aliases: ["ai2", "bot"],
+  cooldown: 0,
 };
 
 module.exports.run = async function ({ api, event, args }) {
-		try {
-				const prompt = args.join(" ");
-				if (!prompt) {
-						await api.sendMessage("Salut je serai ravie de répondre à tes questions .", event.threadID);
-						return;
-				}
+  try {
+    const prompt = args.join(" ");
+    if (!prompt) {
+      await api.sendMessage("Salut je serai ravie de répondre à tes questions", event.threadID);
+      return;
+    }
+    const response = await axios.get(`https://deku-rest-api-3ijr.onrender.com/api/liner?q=${encodeURIComponent(prompt)}`);
+    const answer = response.data.answer;
 
-				const response = await axios.get(`https://deku-rest-api-3ijr.onrender.com/api/liner?q=${encodeURIComponent(prompt)}`);
-				const answer = response.data.answer;
+    let formattedAnswer = "";
+    for (let char of answer) {
+      if (fonts[char.toLowerCase()]) {
+        formattedAnswer += fonts[char.toLowerCase()];
+      } else {
+        formattedAnswer += char;
+      }
+    }
 
-				await api.sendMessage(answer, event.threadID);
-		} catch (error) {
-				console.error("Error:", error.message);
-		}
+    await api.sendMessage(`${formattedAnswer} `, event.threadID);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 };
