ed80a09c244174686bc57a5995fe81c54aef2ba2
Sory Ronald <165383634+SoryRonald@users.noreply.github.com>
2024-08-03 12:24:05 +0000

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,44 +1,33 @@
-const fonts = {
-  a: "𝖺", b: "𝖻", c: "𝖼", d: "𝖽", e: "𝖾", f: "𝖿", g: "𝗀", h: "𝗁",
-  i: "𝗂", j: "𝗃", k: "𝗄", l: "𝗅", m: "𝗆", n: "𝗇", o: "𝗈", 
-  p: "𝗉", q: "𝗊", r: "𝗋", s: "𝗌", t: "𝗍", u: "𝗎", v: "𝗏", 
-  w: "𝗐", x: "𝗑", y: "𝗒", z: "𝗓" 
-};
-
 const axios = require('axios');
-
 module.exports.config = {
-  name: "ai",
-  version: 1.0,
-  credits: "aesther",//Api OtinXsandip
-  description: "AI",
+  name: 'ai',
+  version: '1.0.0',
+  role: 0,
   hasPrefix: false,
-  usages: "{pn} [prompt]",
-  aliases: ["ai2", "bot"],
-  cooldown: 0,
+  aliases: ['gpt', 'openai', 'assistant'],
+  description: "An AI command powered by GPT-4",
+  usage: "Ai [promot]",
+  credits: 'Metoushela Walker',
+  cooldown: 3,
 };
-
-module.exports.run = async function ({ api, event, args }) {
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const input = args.join(' ');
+  if (!input) {
+    api.sendMessage(`Veuillez saisir votre question 🌐`, event.threadID, event.messageID);
+    return;
+  }
+  api.sendMessage(`🔴🟡🟢\n━━━━━━━━━━━\n "${input}"`, event.threadID, event.messageID);
   try {
-    const prompt = args.join(" ");
-    if (!prompt) {
-      await api.sendMessage("Salut je serai ravie de répondre à tes questions", event.threadID);
-      return;
-    }
-    const response = await axios.get(`https://deku-rest-api-3ijr.onrender.com/api/liner?q=${encodeURIComponent(prompt)}`);
-    const answer = response.data.answer;
-
-    let formattedAnswer = "";
-    for (let char of answer) {
-      if (fonts[char.toLowerCase()]) {
-        formattedAnswer += fonts[char.toLowerCase()];
-      } else {
-        formattedAnswer += char;
-      }
-    }
-
-    await api.sendMessage(`${formattedAnswer} `, event.threadID);
+    const {
+      data
+    } = await axios.get(`https://metoushela-rest-api-tp5g.onrender.com/api/gpt4o?context=${encodeURIComponent(input)}`);
+    const response = data.response;
+    api.sendMessage('⚘𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁⊰♔⊱\n\n⊰⊹⊱♡⊰⊹⊱♡⊰⊹⊱♡⊰⊹\n' + response + '\n╰┈➤⊹⊱✫⊰⊹⊱✫⊰🍀', event.threadID, event.messageID);
   } catch (error) {
-    console.error("Error:", error.message);
+    api.sendMessage('🔴 An error occurred while processing your request..', event.threadID, event.messageID);
   }
 };
