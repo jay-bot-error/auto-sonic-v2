d0219e92b1cdd8d5dc8f24105b6d63ee4d048794
Jayden smith <jaybot4278@gmail.com>
2025-01-14 04:29:26 +0200

    Update Developpeur.js

--- Developpeur.js
+++ Developpeur.js
@@ -25,9 +25,9 @@ module.exports.run = async function({ api, event, args }) {
 		const uptimeString = `${years > 0 ? `${years} years ` : ''}${months > 0 ? `${months} months ` : ''}${weeks > 0 ? `${weeks} weeks ` : ''}${days % 7 > 0 ? `${days % 7} days ` : ''}${hours > 0 ? `${hours} hours ` : ''}${minutes > 0 ? `${minutes} minutes ` : ''}${seconds} seconds`;
 
 		const prefix = "🤣";
-		const CREATORLINK = "https://www.facebook.com/profile.php?id=61551417118451";
-		const BOTCREATOR = "Métöushela Walkēr";
-		const BOTNAME = "MEGAN AE";
+		const CREATORLINK = "https://www.facebook.com/profile.php?id=lordjaydenSmith.1";
+		const BOTCREATOR = "Jayden Smith";
+		const BOTNAME = "Jay Bot";
 		const FILESOWNER = "AE";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4",
